b9155ec4e6dfa279e89e43983e206d210bbfeebe
phongsathon-chansongkrao <kitokidandkwa@gmail.com>
2022-09-12 14:02:59 +0700

    test1

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,13 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if(word == null){
+    return undefined
+  }else if(word == undefined){
+    return undefined
+  }else if(startWord == word){
+    return "word"
+  }else 
+    return startWord+word
 }
 module.exports = fillStartWord
